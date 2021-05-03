ed87754c31fc44cb898a847721393e3436a18bd9
Thejas04 <77866242+Thejas04@users.noreply.github.com>
2021-05-03 13:07:02 +0530

    Add files via upload

---
+++ Phase_3_Assessment-main/Phase_3_Assessment-main/thirdproject-main/src/main/java/com/tvi/sportyshoes/SportyShoesByTejasApplication.java
@@ -0,0 +1,13 @@
+package com.tvi.sportyshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyShoesByTejasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyShoesByTejasApplication.class, args);
+	}
+
+}
